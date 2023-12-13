f572c9f694ba43ae3dd1f76e5a95dd90442a2559
Tyuiu.IvashkinaKE@outlook.com <Tyuiu.IvashkinaKE@outlook.com>
2023-12-14 00:04:06 +0500

    Add project files.

---
+++ Tyuiu.IvashkinaKE.Sprint7.Project.V3.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.IvashkinaKE.Sprint7.Project.V3.Lib;
+
+namespace Tyuiu.IvashkinaKE.Sprint7.Project.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        DataService ds = new DataService();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string[,] array = { { "1", "Семёнов Илья Павлович", "Мельникайте 70", "профессор", "01", "дискретная математика", "12", "семинар", "высшая математика", "101", "МиПИТ", "11", "522" },
+                                { "2", "Сергеева Ксения Яновна", "Володарского 38", "лаборант", "02", "ядерная физика", "14", "экзамен", "физика", "202", "МиПИТ", "22", "340" },
+                                { "3", "Андреенко Станислав Аркадьевич", "Республики 47", "декан", "03", "изучение языка Питон", "16", "коллоквиум", "программирование", "303", "МиПИТ", "33", "715" }, };
+        }
+    }
+       
+    }
+
---
+++ Tyuiu.IvashkinaKE.Sprint7.Project.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvashkinaKE.Sprint7.Project.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvashkinaKE.Sprint7.Project.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0298c31d-0f0d-4081-b9bd-3357797404b5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
